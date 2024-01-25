e6fa97e69314f73665ec8c5477a3c2d1bd6eec32
Roman Gellert <titangtace@gmail.com>
2024-01-25 18:54:49 +0100

    Some testing

--- invaders_tutorial_project/src/main.rs
+++ invaders_tutorial_project/src/main.rs
@@ -3,9 +3,22 @@ use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
-    audio.add("explode", "target/explode.wav");
-    audio.play("explode");
 
+    // adding audio 
+    audio.add("explode", "target/explode.wav");
+    audio.add("lose", "target/lose.wav");
+    audio.add("move", "target/move.wav");
+    audio.add("pew", "target/pew.wav");
+    audio.add("startup", "target/startup.wav");
+    audio.add("win", "target/win.wav");
+
+    // test play
+    audio.play("win");
     audio.wait();
+
+    // test play
+    audio.play("explode");
+    audio.wait();
+
     Ok(())
 }
