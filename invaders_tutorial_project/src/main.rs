11b52d489d8e5b9fd813bc731240090d1843ecd4
Roman Gellert <titangtace@gmail.com>
2024-01-26 14:42:19 +0100

    Some basic steps

--- invaders_tutorial_project/src/main.rs
+++ invaders_tutorial_project/src/main.rs
@@ -1,4 +1,5 @@
-use std::error::Error;
+use std::{error::Error, io};
+use crossterm::{cursor::{Hide, Show}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -12,13 +13,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("startup", "target/startup.wav");
     audio.add("win", "target/win.wav");
 
-    // test play
     audio.play("win");
-    audio.wait();
 
-    // test play
-    audio.play("explode");
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Finish cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 
     Ok(())
 }
