a21ac7bc8263f47939bcae7cc3c3595fc4da3068
Roman Gellert <titangtace@gmail.com>
2024-01-27 18:15:22 +0100

    Added game loop

--- invaders_tutorial_project/src/main.rs
+++ invaders_tutorial_project/src/main.rs
@@ -1,5 +1,5 @@
-use std::{error::Error, io};
-use crossterm::{cursor::{Hide, Show}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
+use std::{error::Error, io, time::Duration};
+use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -13,13 +13,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("startup", "target/startup.wav");
     audio.add("win", "target/win.wav");
 
-    audio.play("win");
-
     // Terminal
     let mut stdout = io::stdout();
     terminal::enable_raw_mode()?;
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
+
+
+    // Game loop
+    audio.play("startup");
+
+    'gameloop: loop {
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    },
+                    _ => {}
+                }
+            }
+        }
+    }
 
     // Finish cleanup
     audio.wait();
